e3f5b66f1fd32735322e1159e85c4a411fb8f519
CleversonAbreu <crehbatera@gmail.com>
2021-11-23 19:28:36 -0300

    :tada: initial commit

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,{ Component }from 'react';
+import{ View, Text,TouchableOpacity, StyleSheet,Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      phrase: '',
+      img: require('./assets/cookie.png')
+    }
+
+    this.openCookie = this.openCookie.bind(this)
+
+    this.phrases = [
+      'No matter how thin you slice it, there will always be two sides.',
+      'Everything excellent is as difficult as it is rare.',
+      'Do not weep. Do not wax indignant. Understand.',
+      'The endeavor to understand is the first and only basis of virtue.',
+      'No to laugh, not to lament, not to detest, but to understand.'
+    ];
+  }
+
+  openCookie(){
+    let number =  Math.floor(Math.random() * this.phrases.length)
+
+    this.setState({
+      phrase: '" '+this.phrases[number]+' "',
+      img:  require('./assets/cookie_open.png')
+    })
+    
+    setInterval(() => {
+      this.setState({
+        phrase: '',
+        img:  require('./assets/cookie.png')
+      });
+    }, 4000);
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image source={this.state.img} style={styles.img}/>
+        <Text style={styles.text}>{this.state.phrase}</Text>
+        <TouchableOpacity style={styles.btn} onPress={this.openCookie}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnText}>Open Cookie</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  img:{
+    width: 250,
+    height: 250
+  },
+  text:{
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  btn:{
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  btnArea:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnText:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color:'#dd7b22',
+  }
 });
+
+export default App;
